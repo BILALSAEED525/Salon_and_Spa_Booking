91f76c4ea582614314b6a4b39f528dba9b7feb8f
BILALSAEED525 <bilalsaeed5439@gmail.com>
2026-07-25 16:15:03 +0500

    added real requirements.txt

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -78,7 +78,6 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
-        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
